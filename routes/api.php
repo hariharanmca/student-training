353ccb5a4d328b03c955e03f9ae788b52251af96
hari <90253660+hariharanmca@users.noreply.github.com>
2025-02-13 11:45:42 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,11 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
-    
+    Route::middleware('role:student')->group(function () {
             Route::get('courses', [CourseController::class, 'index']);
             Route::post('student-training/opt-in', [StudentTrainingController::class, 'optIn']);
             Route::post('student-training/opt-out', [StudentTrainingController::class, 'optOut']);
-    
+    });
     
         Route::middleware('role:admin')->group(function () {
             Route::apiResource('courses', CourseController::class);
